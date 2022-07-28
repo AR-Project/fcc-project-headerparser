3f33f031f35f55823fadaaeba2c3b601f3019c68
AR Project <alvenresta@gmail.com>
2022-07-28 20:11:55 +0700

    Fix bug wrong information displayed, adding notes, and cleaning up the code.

--- src/handler.js
+++ src/handler.js
@@ -2,19 +2,23 @@
 // const { request } = require('express')
 
 function whoAmI (req, res) {
+  // Setup
   const header = {};
   const rawHeaders = req.rawHeaders;
+
+  // Parsing data from rawHeader into header object
   for (let i = 0; i < rawHeaders.length; i += 2){
     header[rawHeaders[i]] = rawHeaders[i+1]
   }
 
+  // Wrap result as Object
   const result = {
-    ipaddress: header['Host'],
+    ipaddress: req.ip,
     language: header['Accept-Language'],
     software: header['User-Agent']
   }
 
-  console.log(result)
+  // Returning result object as JSON
   res.json(result)
 };
 
